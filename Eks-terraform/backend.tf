5325ed065422e56ef20aed049862110975677059
your.Upendar <upendarsankoj7@gmail.com>
2023-12-18 23:22:10 +0530

    Updated Eks-files

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eksbucket07" # Replace with your actual S3 bucket name
+    bucket = "eksbucket007" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-west-1"
   }

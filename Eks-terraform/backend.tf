1c89bc06cccd16d9b1178f9777d604b2e6234a6d
upendarsankoju7 <50440972+upendarsankoju7@users.noreply.github.com>
2023-12-18 16:48:17 +0530

    Update backend.tf
    Added

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "eksbucket07" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-southeast-2"
+    region = "us-west-1"
   }
 }
